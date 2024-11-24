e0837e8aba20b535e79e849a3b279b5cc2b3162d
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-11-24 09:13:39 +0500

    updated array methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,29 +1,36 @@
-const students = [  
-    { name: 'Alice', score: 85 },  
-    { name: 'Bob', score: 45 },  
-    { name: 'Charlie', score: 72 },  
-    { name: 'Diana', score: 55 }  
-];  
+const students = [
+  { name: "Akram", score: 85 },
+  { name: "Dil Shad", score: 45 },
+  { name: "Rehan", score: 72 },
+  { name: "Imran", score: 55 },
+];
+
+console.log("some method");
+const hasPassingStudent = students.some((student) => student.score >= 60);
+console.log(`Is there any student with a passing score? ${hasPassingStudent}`); // true
+
+console.log("reduce method");
+const totalScore = students.reduce((sum, student) => {
+  console.log("sum", sum, student.score);
+  return sum + student.score;
+}, 0);
+console.log(`Total score of all students: ${totalScore}`); // 257
+
+console.log("filter method");
+const failingStudents = students
+  .filter((student) => student.score < 60)
+  .map((student) => student.name)
+  .join(", ");
+console.log(`Students with failing scores: ${failingStudents}`); // Bob, Diana
+console.log('=>forEach method')
+students.forEach((student) => {
+  console.log(`${student.name} has a score of ${student.score}`);
+});
 
 
-const hasPassingStudent = students.some(student => student.score >= 60);  
-console.log(`Is there any student with a passing score? ${hasPassingStudent}`); // true  
-
-
-const totalScore = students.reduce((sum, student) => sum + student.score, 0);  
-console.log(`Total score of all students: ${totalScore}`); // 257  
-
-const failingStudents = students.filter(student => student.score < 60)  
-                                 .map(student => student.name)  
-                                 .join(', ');  
-console.log(`Students with failing scores: ${failingStudents}`); // Bob, Diana  
-
-
-students.forEach(student => {  
-    console.log(`${student.name} has a score of ${student.score}`);  
-});  
-
-
-const studentNames = students.map(student => student.name);  
-const isCharlieEnrolled = studentNames.includes('Charlie');  
-console.log(`Is Charlie enrolled in the class? ${isCharlieEnrolled}`); // true
+console.log("=>map method")
+const studentNames = students.map((student) => student.name);
+console.log(studentNames)
+console.log("=> includes method")
+const isCharlieEnrolled = studentNames.includes("Muhammad Shah");
+console.log(`Is Muhammad Shah enrolled in the class? ${isCharlieEnrolled}`); // true
